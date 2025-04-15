00da911702fb4836ea1d0feffd42582654e9f99d
Laerte Henrique Patrocinio <lhpatrocinio@gmail.com>
2025-04-15 01:02:09 -0300

    Configurado swagger com versionamento no projeto

---
+++ Core.PosTech8Nett/src/Core.PosTech8Nett.Api/Infra/Swagger/Filters/CustomSchemaExcludeFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Core.PosTech8Nett.Api.Infra.Swagger.Filters
+{
+    [ExcludeFromCodeCoverage]
+    public class CustomSchemaExcludeFilter : ISchemaFilter
+    {
+        private static readonly List<string> ExcludeProperties = new List<string> { "layer", "applicationName" };
+
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (schema?.Properties == null) { return; }
+
+            ExcludeProperties.ForEach(prop =>
+            {
+                if (schema.Properties.ContainsKey(prop))
+                {
+                    schema.Properties.Remove(prop);
+                }
+            });
+        }
+    }
+}
---
+++ Core.PosTech8Nett/src/Core.PosTech8Nett.Api/Infra/Swagger/SwaggerServicesExtension.cs
@@ -0,0 +1,73 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Core.PosTech8Nett.Api.Infra.Swagger.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+
+namespace Core.PosTech8Nett.Api.Infra.Swagger
+{
+    [ExcludeFromCodeCoverage]
+    public static class SwaggerServicesExtension
+    {
+        /// <summary>
+        /// Add swagger documentation service on dependency injection container 
+        /// </summary>
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>            {
+
+                var _provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                foreach (var description in _provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerDoc(description.GroupName, new OpenApiInfo()
+                    {
+                        Title = $"API - {description.GroupName.ToUpper()}",
+                        Version = description.ApiVersion.ToString(),
+                        Description = "Documentação gerada automaticamente com Swagger e ApiVersioning."                       
+                    });
+                }
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    In = ParameterLocation.Header,
+                    Description = @"Insira o token JWT
+                      \r\n\r\nExample: 'Bearer 12345abcdef'"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+
+                options.SchemaFilter<CustomSchemaExcludeFilter>();
+                options.DocumentFilter<LowerCaseDocumentFilter>();
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+    }
+}
