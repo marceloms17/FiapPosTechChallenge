b22e049917c289c343dafa612f10250cf5f11d88
Laerte Henrique Patrocinio <lhpatrocinio@gmail.com>
2025-04-16 09:06:00 -0300

    adicionado jwt e endpoint de geracao de token

---
+++ Core.PosTech8Nett/src/Core.PosTech8Nett.Api/Controllers/V1/AuthenticationController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using Asp.Versioning;
+
+namespace Core.PosTech8Nett.Api.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("Token")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (model.Username != "admin" || model.Password != "123456")
+                return Unauthorized();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("sua-chave-secreta-bem-grande-aqui");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, model.Username),
+                new Claim(ClaimTypes.Role, "Admin")
+            }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwt = tokenHandler.WriteToken(token);
+
+            return Ok(new { token = jwt });
+        }
+    }
+
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Core.PosTech8Nett/src/Core.PosTech8Nett.Api/Infra/Auth/AuthorizationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Core.PosTech8Nett.Api.Infra.Auth
+{
+    [ExcludeFromCodeCoverage]
+    public static class AuthorizationExtensions
+    {
+        public static void AddAuthorizationExtension(this IServiceCollection services)
+        {
+            var key = "sua-chave-secreta-bem-grande-aqui";
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false; 
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,    
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                };
+            });
+
+            services.AddAuthorization();
+        }
+    }
+}
