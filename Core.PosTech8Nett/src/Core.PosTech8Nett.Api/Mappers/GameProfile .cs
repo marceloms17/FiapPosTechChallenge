370197f8636fba93bfca65ff4a651a243a36d466
Marcelo Morais <marceloms17@gmail.com>
2025-05-28 19:20:37 -0300

    feat(mapper): configurar mapeamento entre Game e GameRequest/GameResponse

---
+++ Core.PosTech8Nett/src/Core.PosTech8Nett.Api/Mappers/GameProfile .cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.PosTech8Nett.Api.Domain.Entities.GameInformation;
+using Core.PosTech8Nett.Api.Domain.Model.Game;
+using System.Linq;
+
+namespace Core.PosTech8Nett.Api.Mappers
+{
+    public class GameProfile : Profile
+    {
+        public GameProfile()
+        {
+            CreateMap<Game, GameResponse>()
+                .ForMember(dest => dest.Genres, opt => opt.MapFrom(src =>
+                    src.Genres.Select(g => g.GenreType.Description)));
+
+            CreateMap<GameRequest, Game>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Genres, opt => opt.Ignore()); // será tratado manualmente
+        }
+    }
+
+}
